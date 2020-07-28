89006383ba11916db997b72a0d4d151ad9eeab08
Zidan <b93605079@g.ntu.edu.tw>
2020-07-28 16:26:51 +0800

    JS寫滾動事件、spy

--- js/js.js
+++ js/js.js
@@ -29,3 +29,45 @@ function op(x,y,url) {
 function cl(x) {
 	$(x).fadeOut();
 }
+
+//Jquery寫法
+//scroll to specific id
+$(document).ready(function(){
+
+	$("#mynavbar a, #scrollToBanner").click(function(){
+		// console.log(this);
+		let who=$(this).attr("href");
+		// console.log($(who).offset().top);
+		let val=$(who).offset().top;
+		$("html").animate({
+			scrollTop : val
+		},1000,"swing");
+	});
+	
+	// scroll spy
+	$(window).scroll(function(){ spy() });
+		spy();
+	
+
+	function spy(){
+		let nowAt=$(window).scrollTop();
+
+		$("section").each(function(){
+			let 
+				id=$(this).attr("id"),
+				offset=$(this).offset().top,
+				height=$(this).innerHeight();
+			
+				if(offset<nowAt && nowAt<offset+height){
+					// this section is nowAt's zone
+					$("#mynavbar a").removeClass("active");
+					$(`#mynavbar a[href='#${id}']`).addClass("active");
+				}
+
+		});
+
+	}
+
+
+});
+
